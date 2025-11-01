578b9bdf3251d407872a52e9248c5ca6baf10c61
Anusha Yuvika <yuvikaanusha19@gmail.com>
2025-11-01 17:03:36 +0530

    submition 1

--- src/components/App.js
+++ src/components/App.js
@@ -9,46 +9,73 @@ class App extends Component {
       posi: 0,
       ballPosition: { left: "0px" },
     };
-    this.renderChoice = this.renderBallOrButton.bind(this);
+    // this.renderChoice = this.renderBallOrButton.bind(this);
+    // this.buttonClickHandler = this.buttonClickHandler.bind(this);
+    // this.handleKeyDown = this.handleKeyDown.bind(this);
+
+    this.renderChoice = this.renderChoice.bind(this);
     this.buttonClickHandler = this.buttonClickHandler.bind(this);
-    this.handleKeyDown = this.handleKeyDown.bind(this);
   }
 
   buttonClickHandler() {
-    this.setState({ renderBall: true }, () => {
-      document.addEventListener("keydown", this.handleKeyDown);
-    });
+    this.setState({ renderBall: true });
   }
 
-  handleKeyDown(event) {
-    if (event.key === "ArrowRight" && this.state.renderBall) {
-      this.setState({
-        posi: this.state.posi + 5,
-        ballPosition: { left: this.state.posi + 5 + "px" },
-      });
-    }
-  }
-
-  renderBallOrButton() {
+  renderChoice() {
     if (this.state.renderBall) {
-      console.log("this.state.ballPosition", this.state.ballPosition);
       return <div className="ball" style={this.state.ballPosition}></div>;
     } else {
       return (
         <button className="start" onClick={this.buttonClickHandler}>
-          Start
+          Click For One Ball
         </button>
       );
     }
   }
 
+  //   buttonClickHandler() {
+  //     this.setState({ renderBall: true }, () => {
+  //       document.addEventListener("keydown", this.handleKeyDown);
+  //     });
+  //   }
+
+  //   handleKeyDown(event) {
+  //     if (event.key === "ArrowRight" && this.state.renderBall) {
+  //       this.setState({
+  //         posi: this.state.posi + 5,
+  //         ballPosition: { left: this.state.posi + 5 + "px" },
+  //       });
+  //     }
+  //   }
+
+  //   renderBallOrButton() {
+  //     if (this.state.renderBall) {
+  //       console.log("this.state.ballPosition", this.state.ballPosition);
+  //       return <div className="ball" style={this.state.ballPosition}></div>;
+  //     } else {
+  //       return (
+  //         <button className="start" onClick={this.buttonClickHandler}>
+  //           Start
+  //         </button>
+  //       );
+  //     }
+  //   }
+
   // bind ArrowRight keydown event
   componentDidMount() {
-    document.addEventListener("keydown", this.handleKeyDown);
+    // document.addEventListener("keydown", this.handleKeyDown);
+    document.addEventListener("keydown", (e) => {
+      if (e.keyCode === 39) {
+        this.setState({
+          ballPosition: { left: this.state.posi + 5 + "px" },
+          posi: this.state.posi + 5,
+        });
+      }
+    });
   }
 
   render() {
-    return <div className="playground">{this.renderBallOrButton()}</div>;
+    return <div className="playground">{this.renderChoice()}</div>;
   }
 }
 
